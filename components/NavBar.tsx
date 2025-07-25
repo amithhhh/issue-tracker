d23fe037014f89fd1a98b74a3b2ff1bef598454f
AMITH E K <amithajith7025@gmail.com>
2025-07-25 11:00:21 +0530

    styled the active link in navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,25 +1,35 @@
+'use client';
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { DiBugsense } from "react-icons/di";
+import classnames from 'classnames';
 
 const NavBar = () => {
 
+  const currentPath = usePathname();
   const links = [
-    {label: 'Dashboard', href: '/'},
-    {label: 'issues', href: '/issues'},
+    { label: 'Dashboard', href: '/' },
+    { label: 'issues', href: '/issues' },
 
   ]
 
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><DiBugsense className='w-15 h-15' /></Link>
-        <ul className='flex space-x-6'>
-          {
-            links.map((item, index) => (
-                          <li key={index}><Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href={item.href}>{item.label}</Link></li>
-            ))
-          }
-        </ul>
+      <Link href="/"><DiBugsense className='w-15 h-15' /></Link>
+      <ul className='flex space-x-6'>
+        {
+          links.map((item, index) => (
+            <li key={index}><Link className={classnames({
+              'text-zinc-900': item.href === currentPath,
+              'text-zinc-500': item.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true
+
+            })} href={item.href}>{item.label}</Link></li>
+          ))
+        }
+      </ul>
     </nav>
   )
 }
