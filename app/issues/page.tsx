a440f12bdcb35f45c9707dcaae62e46508b5b689
AMITH E K <amithajith7025@gmail.com>
2025-07-25 14:35:54 +0530

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes'
 
 const page = () => {
   return (
-    <div> Issues page</div>
+    <div><Button>New Issue</Button></div>
   )
 }
 
